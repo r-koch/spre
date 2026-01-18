96b4945380d80bc3882c16f7adf0ce7b56db9f5e
r-koch <me@rkoch.dev>
2026-01-18 14:30:43 +0100

    clarified ec2 commands

--- ec2/commands.sh
+++ ec2/commands.sh
@@ -1,14 +1,17 @@
+# in wsl window 1 - connect to instance
 ssh -i .ssh/spre-training.pem ubuntu@34.250.200.236
 
 source /opt/tensorflow/bin/activate
 
 pip install pyarrow boto3 botocore numpy
 
+# in wsl window 2 - copy code
 rsync -avz --delete \
   -e "ssh -i ~/.ssh/spre-training.pem" \
   ~/spre/python/ \
   ubuntu@34.250.200.236:~/python/
 
+# in wsl window 1 - start training
 cd ~/python
 
 python training.py
