ddabbe85947f2aef3eb3b460e7a485cd48ef20ee
r-koch <me@rkoch.dev>
2026-01-18 15:19:52 +0100

    exclude __pycache__

--- ec2/commands.sh
+++ ec2/commands.sh
@@ -1,5 +1,5 @@
 # in wsl window 1 - connect to instance
-ssh -i .ssh/spre-training.pem ubuntu@34.250.200.236
+ssh -i .ssh/spre-training.pem ubuntu@18.201.12.105
 
 source /opt/tensorflow/bin/activate
 
@@ -7,9 +7,11 @@ pip install pyarrow boto3 botocore numpy
 
 # in wsl window 2 - copy code
 rsync -avz --delete \
+  --exclude='__pycache__/' \
+  --exclude='*.pyc' \
   -e "ssh -i ~/.ssh/spre-training.pem" \
   ~/spre/python/ \
-  ubuntu@34.250.200.236:~/python/
+  ubuntu@18.201.12.105:~/python/
 
 # in wsl window 1 - start training
 cd ~/python
